d2e9927a5a465f03f6dbf2e6aadb602579cec625
Der_Googler <jimmyboehm@dergoogler.com>
2022-06-27 22:53:09 +0200

    Update `render` to latest react api

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import ReactDOM from "react-dom";
 
 // Styles
 import "./styles/default.scss";
 
-ReactDOM.render(<App />, document.querySelector("app"));
+// Setup root node where our React app will be attached to
+const app = document.createElement("app");
+document.body.appendChild(app);
+
+// Render the app component
+const container = document.querySelector("app");
+const root = createRoot(container!);
+root.render(<App />);
